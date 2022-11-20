bf22af0a5e90b7ababe7d1bea7e998e1fbc249fe
PeZunino <pedrozsantos@hotmail.com>
2022-11-20 09:30:59 -0300

    input blur and focus

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import { Container } from './style';
+import React, { useCallback, useState } from 'react';
+import { Container, Shadow } from './style';
 import { IoIosSearch } from 'react-icons/io';
 const Input: React.FC = () => {
+  const [shadowDoc, setShadowDoc] = useState(false);
+  const handleBlur = useCallback(() => {
+    setShadowDoc(false);
+  }, []);
+  const handleFocus = useCallback(() => {
+    setShadowDoc(true);
+  }, []);
+
   return (
-    <Container>
-      <input placeholder="busque aqui seu produto" type="text" />
-      <IoIosSearch className="search-icon" size={30} />
-    </Container>
+    <>
+      <Container>
+        <input
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          placeholder="busque aqui seu produto"
+          type="text"
+        />
+        <IoIosSearch size={30} />
+      </Container>
+      <Shadow activate={shadowDoc} />
+    </>
   );
 };
 
--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import { PropsWithChildren } from 'react';
+import styled, { css } from 'styled-components';
 
+interface ShadowDiv {
+  activate: boolean;
+}
 export const Container = styled.div`
   background-color: white;
   width: 611px;
@@ -12,11 +16,33 @@ export const Container = styled.div`
   padding: 11px 12px 11px 16px;
 
   box-shadow: rgba(0, 0, 0, 0.12) 0px 10px 15px;
+  z-index: 2;
   input {
     width: inherit;
   }
-  .search-icon {
+  svg {
     color: red;
     margin-left: 5px;
   }
+`;
+
+export const Shadow = styled.div<PropsWithChildren<ShadowDiv>>`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.4);
+  transition: all 0.3s;
+  visibility: hidden;
+  opacity: 0;
+  z-index: 0;
+
+  ${props =>
+    props.activate &&
+    css`
+      opacity: 1;
+      visibility: visible;
+      z-index: 1;
+    `}
 `;
